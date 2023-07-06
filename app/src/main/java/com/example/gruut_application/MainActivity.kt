84d04dfa7bb687cf43501e2de7a23db6de5326ed
Junghye Choi <65644118+bigtreeaa@users.noreply.github.com>
2023-07-07 05:53:46 +0900

    Splash Page

--- app/src/main/java/com/example/gruut_application/MainActivity.kt
+++ app/src/main/java/com/example/gruut_application/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.gruut_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed(Runnable {
+            val introIntent = Intent(this@MainActivity, SignupActivity::class.java)
+            startActivity(introIntent)
+        }, 4000)
     }
 }
